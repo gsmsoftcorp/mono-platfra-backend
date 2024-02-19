cff3a4c9c1b3170929f62e3c18b2f2930e9481a1
bearsu <bearsu98@gmail.com>
2024-02-19 12:57:39 +0900

    내 플랫폼 조회

--- platfra/src/main/java/com/gsm/platfra/api/services/platfra/service/ContentSaveService.java
+++ platfra/src/main/java/com/gsm/platfra/api/services/platfra/service/ContentSaveService.java
@@ -7,7 +7,9 @@ import com.gsm.platfra.api.data.platfra.TPlatfraRepository;
 import com.gsm.platfra.api.data.platfra.saved.ContentSave;
 import com.gsm.platfra.api.data.platfra.saved.ContentSaveDto;
 import com.gsm.platfra.api.data.platfra.saved.ContentSaveRepository;
+import com.gsm.platfra.api.services.platfra.dto.PlatformDto;
 import com.gsm.platfra.api.services.platfra.query.ContentSaveQueryRepository;
+import com.gsm.platfra.exception.ExceptionCode;
 import com.gsm.platfra.system.security.context.UserContextUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -29,7 +31,7 @@ public class ContentSaveService {
     @Transactional
     public void saveContent(String platfraId, Long platfraBoardSeq, Long contentSeq) {
         TPlatfra tPlatfra = tPlatfraRepository.findTPlatfraByPlatfraId(platfraId)
-            .orElseThrow(() -> new IllegalArgumentException("해당 플랫폼이 존재하지 않습니다."));
+                .orElseThrow(() -> new IllegalArgumentException(ExceptionCode.NOT_FOUND_PLATFORM.getMessage()));
         String userId = UserContextUtil.getUserContext().getUserId();
 
         ContentSave contentSave = ContentSave.builder()
@@ -45,7 +47,7 @@ public class ContentSaveService {
     @Transactional
     public void saveContent(String platfraId, Long contentSeq) {
         TPlatfra tPlatfra = tPlatfraRepository.findTPlatfraByPlatfraId(platfraId)
-            .orElseThrow(() -> new IllegalArgumentException("해당 플랫폼이 존재하지 않습니다."));
+                .orElseThrow(() -> new IllegalArgumentException(ExceptionCode.NOT_FOUND_PLATFORM.getMessage()));
         String userId = UserContextUtil.getUserContext().getUserId();
 
         ContentSave contentSave = ContentSave.builder()
@@ -59,9 +61,9 @@ public class ContentSaveService {
     }
 
     public List<ContentSaveDto> getMyContentList(String userId) {
-        TAccount tAccount = tAccountRepository.findByUserId(userId).orElseThrow(() -> new IllegalArgumentException("해당 유저가 존재하지 않습니다."));
+        TAccount tAccount = tAccountRepository.findByUserId(userId).orElseThrow(() -> new IllegalArgumentException(ExceptionCode.NOT_FOUND_USER.getMessage()));
         if (!tAccount.getUserId().equals(UserContextUtil.getUserContext().getUserId())) {
-            throw new IllegalCallerException("해당 유저의 권한이 없습니다.");
+            throw new IllegalCallerException(ExceptionCode.AUTHORIZATION_FAILED.getMessage());
         }
 
         return contentSaveQueryRepository.getMyContentList(userId);
@@ -69,10 +71,22 @@ public class ContentSaveService {
 
     @Transactional
     public void deleteContent(String userId, Long contentSaveSeq) {
-        TAccount tAccount = tAccountRepository.findByUserId(userId).orElseThrow(() -> new IllegalArgumentException("해당 유저가 존재하지 않습니다."));
+        TAccount tAccount = tAccountRepository.findByUserId(userId).orElseThrow(() -> new IllegalArgumentException(ExceptionCode.NOT_FOUND_USER.getMessage()));
         if (!tAccount.getUserId().equals(UserContextUtil.getUserContext().getUserId())) {
-            throw new IllegalCallerException("해당 유저의 권한이 없습니다.");
+            throw new IllegalCallerException(ExceptionCode.AUTHORIZATION_FAILED.getMessage());
         }
         contentSaveQueryRepository.deleteContent(contentSaveSeq);
+    }
+
+    public List<PlatformDto> getMyPlatform() {
+        String userId = UserContextUtil.getUserContext().getUserId();
+        TAccount tAccount = tAccountRepository.findByUserId(userId).orElseThrow(() -> new IllegalArgumentException(ExceptionCode.NOT_FOUND_USER.getMessage()));
+
+        List<TPlatfra> myPlatforms = tPlatfraRepository.findAllByOwnerId(userId);
+        List<PlatformDto> response = myPlatforms.stream()
+                .map(PlatformDto::from)
+                .toList();
+
+        return response;
     }
 }
--- platfra/src/main/java/com/gsm/platfra/exception/ExceptionCode.java
+++ platfra/src/main/java/com/gsm/platfra/exception/ExceptionCode.java
@@ -8,10 +8,21 @@ public enum ExceptionCode {
     HTTP_METHOD_NOT_SUPPORTED("HR001", "지원하지 않는 HTTP Method 입니다."),
     VALIDATION_PARAMETER_ERROR("VR001", "파라미터가 유효하지 않습니다."),
     DATA_INTEGRITY_VIOLATION_EXCEPTION("DR001", "DB 데이터 베이스 무결성을 위반했습니다."),
+
+
     AUTHENTICATION_FAILED("AR001", "인증에 실패하였습니다."),
+    AUTHORIZATION_FAILED("AR002", "권한이 없습니다."),
+
     ENTITY_NOT_FOUND("ER001", "엔티티가 존재하지 않습니다."),
     BUSINESS_LOGIC_ERROR("BR001", "비즈니스 로직 실행 중 오류가 발생했습니다."),
-    SERVER_ERROR("SR001", "서버 오류가 발생했습니다.")
+    SERVER_ERROR("SR001", "서버 오류가 발생했습니다."),
+
+    NOT_FOUND_PLATFORM("TT", "해당 플랫폼이 존재하지 않습니다."),
+    NOT_FOUND_USER("TU", "해당 유저가 존재하지 않습니다."),
+    NOT_FOUND_CONTENT("TC", "해당 컨텐츠가 존재하지 않습니다."),
+    NOT_FOUND_BOARD("TB", "해당 게시판이 존재하지 않습니다."),
+
+
     ;
     private final String code;
     private final String message;
