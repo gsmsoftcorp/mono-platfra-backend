4ce91d4178e38388d44b6a1aa51bc268c3c942df
bearsu <bearsu98@gmail.com>
2024-02-19 14:51:58 +0900

    구독 플랫폼 조회

--- platfra/src/test/java/com/gsm/platfra/common/util/DateUtilsTest.java
+++ platfra/src/test/java/com/gsm/platfra/common/util/DateUtilsTest.java
@@ -1,9 +1,10 @@
 package com.gsm.platfra.common.util;
 
+import com.gsm.platfra.util.DateUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DateUtilsTest {
 
