1b4c88087aefce1a27b82ca15618ba7d03d13f7c
joo-chang <jooc0311@gmail.com>
2024-02-26 21:48:32 +0900

    account, file id 컬럼 추가 진행 중

--- platfra/src/main/java/com/gsm/platfra/api/services/account/info/service/AccountInfoService.java
+++ platfra/src/main/java/com/gsm/platfra/api/services/account/info/service/AccountInfoService.java
@@ -1,8 +1,12 @@
 package com.gsm.platfra.api.services.account.info.service;
 
+import com.gsm.platfra.api.common.file.repository.TCommonFileRepository;
+import com.gsm.platfra.api.data.account.TAccount;
+import com.gsm.platfra.api.data.account.TAccountRepository;
 import com.gsm.platfra.api.data.account.info.AccountInfoDto;
 import com.gsm.platfra.api.data.account.info.AccountInfoRepository;
 import com.gsm.platfra.api.data.account.info.TAccountInfo;
+import com.gsm.platfra.api.data.common.file.TCommonFile;
 import com.gsm.platfra.api.services.account.info.query.AccountInfoQueryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,10 +20,16 @@ import org.springframework.transaction.annotation.Transactional;
 public class AccountInfoService {
     private final AccountInfoRepository accountInfoRepository;
     private final AccountInfoQueryRepository accountInfoQueryRepository;
+    private final TAccountRepository tAccountRepository;
+    private final TCommonFileRepository tCommonFileRepository;
 
     @Transactional
     public void create(AccountInfoDto accountInfoDto) {
+        TAccount tAccount = tAccountRepository.findById(accountInfoDto.getUserId()).orElseThrow();
+        TCommonFile tCommonFile = tCommonFileRepository.findById(accountInfoDto.getFileSeq()).orElseThrow();
+        accountInfoDto.setUserInfo(tAccount, tCommonFile);
         TAccountInfo accountInfo = AccountInfoDto.toEntity(accountInfoDto);
+
         accountInfoRepository.save(accountInfo);
     }
 
