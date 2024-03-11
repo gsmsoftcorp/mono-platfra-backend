d3fd1158dc69030183f3cd86dc19af5945fb0d96
kim.s.d <kimsd1206@naver.com>
2024-03-11 21:52:26 +0900

    상세조회 추가

--- platfra/src/main/java/com/gsm/platfra/api/services/platfra/service/PlatfraService.java
+++ platfra/src/main/java/com/gsm/platfra/api/services/platfra/service/PlatfraService.java
@@ -26,6 +26,26 @@ public class PlatfraService {
     private final TPlatfraRepository tPlatfraRepository;
     private final PlatfraQueryRepository platfraQueryRepository;
     
+    public PlatfraMainDto get(String platfraId) {
+        // note : 여러개의 데이터를 조합하여 응답할때는 [테이블명]DTO 즉, 테이블용 DTO로 조회하여 service 단에서 화면출력용 DTO에 세팅한다.
+        PlatfraMainDto platfraMainDto = new PlatfraMainDto();
+        
+        //1. 플랫폼 마스터 조회
+        TPlatfra tPlatfra = tPlatfraRepository.findByPlatfraId(platfraId).orElseThrow(/*DataNotFoundException::new*/);
+        PlatfraDto platfraDto = PlatfraDto.of(tPlatfra);
+        platfraMainDto.setPlatfraDto(platfraDto);
+        
+        //2. 최신 컨텐츠 조회
+        List<PlatfraContentDto> latestContentList = platfraQueryRepository.getLatestContentList(platfraDto.getPlatfraId());
+        platfraMainDto.setLatestContentList(latestContentList);
+        
+        //3. 인기 컨텐츠 조회
+        List<PlatfraContentDto> hotContentList = platfraQueryRepository.getHotContentList(platfraDto.getPlatfraId());
+        platfraMainDto.setHotContentList(hotContentList);
+        
+        return platfraMainDto;
+    }
+    
     public List<PlatfraDto> getList(PlatfraDto platfraDto) {
         UserContext userContext = UserContextUtil.getUserContext();
         log.debug("userContext : {}", userContext);
